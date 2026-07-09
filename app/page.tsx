b361e1a83e545faf8dabf3daac6206e1e4dd15ab
Seong Joon Lee <cryslub@hotmail.com>
2026-07-09 12:02:18 +0900

    main page change

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { GoogleSignInButton } from '@/app/ui/login-button';
 export default function Home() {
   return (
     <div className="flex flex-1 flex-col items-center justify-center min-h-screen bg-zinc-50 dark:bg-zinc-900">
+      <span className="text-sm text-zinc-500 dark:text-zinc-400 mb-2">Plan and Manage your</span>
       <span className="text-4xl font-semibold tracking-tight text-zinc-800 dark:text-zinc-100 mb-6">Journey</span>
       <GoogleSignInButton />
     </div>
